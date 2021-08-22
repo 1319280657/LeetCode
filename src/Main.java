e4f25ff9ddac1c433e03c65fe7f33cb2b3586623
1319280657@qq.com <1319280657@qq.com>
2021-08-22 22:46:58 +0800

    LeetCode 练习

---
+++ src/Main.java
@@ -0,0 +1,85 @@
+import text.*;
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+       /* Solution solution = new Solution();
+        solution.test();*/
+
+        /*MedianOfTwoSortedArrays medianOfTwoSortedArrays = new MedianOfTwoSortedArrays();
+        medianOfTwoSortedArrays.test();*/
+
+       /* SumOfTwoNumbers sumOfTwoNumbers = new SumOfTwoNumbers();
+        sumOfTwoNumbers.test();*/
+
+     /*   AddTwoNumber addTwoNumber = new AddTwoNumber();
+        addTwoNumber.test();*/
+
+       /* MakeTheXorOfAllSegmentsEqualToZero makeTheXorOfAllSegmentsEqualToZero = new MakeTheXorOfAllSegmentsEqualToZero();
+        makeTheXorOfAllSegmentsEqualToZero.test();*/
+
+        /*LongestValidParentheses longestValidParentheses = new LongestValidParentheses();
+        longestValidParentheses.test();*/
+
+/*
+        LongestSubstringWithoutRepeatingCharacters longestSubstringWithoutRepeatingCharacters = new LongestSubstringWithoutRepeatingCharacters();
+        longestSubstringWithoutRepeatingCharacters.test();*/
+      /*  LongerContiguousSegmentsOfOnesThanZeros longerContiguousSegmentsOfOnesThanZeros = new LongerContiguousSegmentsOfOnesThanZeros();
+        longerContiguousSegmentsOfOnesThanZeros.test();*/
+
+       /* LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
+        longestPalindromicSubstring.test();*/
+
+        /*ZigzagConversion zigzagConversion = new ZigzagConversion();
+        zigzagConversion.test();*/
+
+       /* ReverseInteger reverseInteger = new ReverseInteger();
+        reverseInteger.test();*/
+       /* HammingDistance hammingDistance = new HammingDistance();
+        hammingDistance.test();*/
+
+      /*  StringToIntegerAtoi stringToIntegerAtoi = new StringToIntegerAtoi();
+        stringToIntegerAtoi.test();*/
+/*
+        PalindromeNumber palindromeNumber = new PalindromeNumber();
+        palindromeNumber.test();*/
+
+     /*   ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+        containerWithMostWater.test();*/
+
+    /*    LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        longestCommonPrefix.test();*/
+
+       /* LetterCombinationsOfAPhoneNumber letterCombinationsOfAPhoneNumber = new LetterCombinationsOfAPhoneNumber();
+        letterCombinationsOfAPhoneNumber.test();*/
+
+  /*      PowerOfFour powerOfFour = new PowerOfFour();
+        powerOfFour.test();*/
+       /* PowerOfTwo powerOfTwo = new PowerOfTwo();
+        powerOfTwo.test();*/
+/*
+        PowerOfThree powerOfThree = new PowerOfThree();
+        powerOfThree.test();*/
+
+      /*  ContinuousSubarraySum continuousSubarraySum = new ContinuousSubarraySum();
+        continuousSubarraySum.test();*/
+       /* CanYouEatYourFavoriteCandyOnYourFavoriteDay canYouEatYourFavoriteCandyOnYourFavoriteDay = new CanYouEatYourFavoriteCandyOnYourFavoriteDay();
+        canYouEatYourFavoriteCandyOnYourFavoriteDay.test();*/
+
+
+     /*   ContiguousArray contiguousArray = new ContiguousArray();
+        contiguousArray.test();*/
+       /* IntersectionOfTwoLinkedLists intersectionOfTwoLinkedLists = new IntersectionOfTwoLinkedLists();
+        intersectionOfTwoLinkedLists.test();*/
+   /*     FirstMissingPositive firstMissingPositive = new FirstMissingPositive();
+        firstMissingPositive.test();*/
+        RemoveLinkedListElements removeLinkedListElements = new RemoveLinkedListElements();
+        removeLinkedListElements.test();
+
+
+    }
+}
